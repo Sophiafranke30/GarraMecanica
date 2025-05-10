b780a61f977265fade16dcd75016993aa923f547
Sophiafranke30 <fra23030@uvg.edu.gt>
2025-05-10 17:21:15 -0600

    Archivo de Arduino
    Se agregó el archivo del código de Arduino

---
+++ Proyecto_GM.ino
@@ -0,0 +1,13 @@
+//Sophia Franke 23030
+//Dulce Ovando 23441
+//Proyecto 2 | Garra Mecánica
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
