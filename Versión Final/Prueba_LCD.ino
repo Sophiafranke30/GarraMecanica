69cc3f0754954a6c466863c5a8d6b7168a282832
Sophia Franke <fra23030@uvg.edu.gt>
2025-05-24 20:59:48 -0600

    Prueba_LCD.ino
    Código de prueba de la pantalla LCD :)

---
+++ Versión Final/Prueba_LCD.ino
@@ -0,0 +1,16 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(9,10,11,12,A4);
+
+void setup()
+{
+  lcd.begin(20,4);
+  lcd.clear();
+  lcd.setCursor(0,0);  lcd.print("Linea 1");
+  lcd.setCursor(0,1);  lcd.print("Linea 2");
+  lcd.setCursor(0,2);  lcd.print("Linea 3");
+  lcd.setCursor(0,3);  lcd.print("Linea 4");
+}
+
+void loop(){}
+
+
